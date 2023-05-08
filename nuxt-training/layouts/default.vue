e26fe7082ecbf28a9257b590d85a52532ea00ed0
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-05-08 16:34:39 +0300

    add prettier

--- nuxt-training/layouts/default.vue
+++ nuxt-training/layouts/default.vue
@@ -1,21 +1,34 @@
 <template>
-    <div>
-<ul>
-    <li><NuxtLink to="/">home</NuxtLink> </li>
-    <li><NuxtLink to="/products">products</NuxtLink></li>
-    <li @click="$router.push('/products/pushdeneme')"> id</li>
-</ul>
-    </div>
+  <div>
+    <ul>
+      <li><NuxtLink to="/">home</NuxtLink></li>
+      <li><NuxtLink to="/products">products</NuxtLink></li>
+      <li><NuxtLink to="/products/pushdeneme"> id</NuxtLink></li>
+    </ul>
+  </div>
 
-    <!-- output the pages content here -->
-    <div>
-        <slot/>
+  <!-- output the pages content here -->
+  <div>
+    <slot />
+  </div>
+
+  <footer>
+    <div
+      style="
+        background-color: rgba(81, 7, 19, 0.75);
+        color: rgb(255, 255, 255);
+        display: flex;
+        justify-content: center;
+      "
+    >
+      This is footer
     </div>
+  </footer>
 </template>
 <style scoped>
-.router-link-exact-active{
-    color: brown;
-    font-size: large;
-    font-weight: bold;
+.router-link-exact-active {
+  color: brown;
+  font-size: large;
+  font-weight: bold;
 }
 </style>
--- nuxt-training/pages/index.vue
+++ nuxt-training/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
-<h1>This is home</h1>
+  <h1>This is home</h1>
 </template>
 
-<script setup lang="ts">
-
-</script>
-
+<script setup lang="ts"></script>
